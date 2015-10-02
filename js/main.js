f0aedbafd4c8c1c3d84aecd69e3146ae3e997fd9
aalinas <angelaalinas@gmail.com>
2015-10-02 14:51:44 -0400

    A descriptive message

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
 
+
+	$('.switch').on('click', function(e) {
+
+		$ ('.light').addClass('dark');
+
+	});
+
+	$('button').on ('click',function(e) {
+
+	$('.status').text('Hey, who turn off the lights?');
+
+	});
+
+
+
+
+	
